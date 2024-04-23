9d74c492a0dcf213e0377d34e5530ad19936f4fb
Teymur <melikovteymur@mail.ru>
2024-04-24 00:01:13 +0400

    mobile adaptability

--- src/js/main.js
+++ src/js/main.js
@@ -10,20 +10,26 @@ $(document).ready(function(){
 
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-      });
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
 
-      function toggleSlide(item) {
+    function toggleSlide(item) {
         $(item).each(function(i) {
             $(this).on('click', function(e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
             })
-          });
-      }
+        });
+    }
 
-      toggleSlide('.catalog-item__link')
-      toggleSlide('.catalog-item__back-link')
+    toggleSlide('.catalog-item__link')
+    toggleSlide('.catalog-item__back-link')
+
+    $('ul.menu__list').on('click', 'li:not(.menu__list-item_active)', function() {
+        $(this)
+        .addClass('menu__list-item_active').siblings().removeClass('menu__list-item_active')
+        // .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
 });
